16c5a8e198648729926af153d564a2bf1830b867
luke0630 <ryuto-0915@outlook.jp>
2024-12-31 22:23:40 +0900

    update: iroiro

--- src/main/java/org/luke/newYearCountDown/EventManager.java
+++ src/main/java/org/luke/newYearCountDown/EventManager.java
@@ -1,32 +1,45 @@
 package org.luke.newYearCountDown;
 
+import org.bukkit.Material;
+import org.bukkit.Sound;
 import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
+import org.bukkit.event.block.BlockBreakEvent;
 import org.bukkit.event.player.PlayerJoinEvent;
 import org.luke.takoyakiLibrary.TakoUtility;
-
-import java.time.Duration;
 
 public class EventManager implements Listener{
     @EventHandler
     public void onPlayerJoin(PlayerJoinEvent event) {
         Player player = event.getPlayer();
 
-        int nextYear = UtilityClass.getNextNewYear().getYear();
-        Duration leftTime = UtilityClass.getLeftTime();
-
-        long left_hours = leftTime.toHours();
-        long left_minutes = leftTime.toMinutes();
-
-        String builder = nextYear + "まで あと" +
-                left_hours + "時間" + ":" +
-                left_minutes + "分";
-
-
-        player.sendMessage(
-                TakoUtility.toColor(builder)
+        player.playSound(
+                player.getLocation(),                 // 再生する位置（プレイヤーの位置）
+                Sound.ENTITY_PLAYER_LEVELUP, // サウンドの名前
+                1.0f,                                 // 音量
+                1.0f                                  // ピッチ
         );
+        player.sendMessage(
+                TakoUtility.toColor("&c&l"+NewYearCountDown.time.getYear() + "年までのカウントダウン待機所です！")
+        );
+        player.sendMessage(
+                TakoUtility.toColor("&a&l好きに建築してください！！！ただし、荒らしは禁止です")
+        );
+        if(NewYearCountDown.getLeftMillis().isNegative()) {
+            player.sendMessage(
+                    TakoUtility.toColor("&6あけおめ～～～～")
+            );
+        }
     }
-
+    @EventHandler
+    public void onBlockBreak(BlockBreakEvent event) {
+        Material type = event.getBlock().getType();
+        if (type == Material.BARRIER || type == Material.BEDROCK) {
+            Player player = event.getPlayer();
+            if (!player.isOp()) {
+                event.setCancelled(true);
+            }
+        }
+    }
 }
--- src/main/java/org/luke/newYearCountDown/NewYearCountDown.java
+++ src/main/java/org/luke/newYearCountDown/NewYearCountDown.java
@@ -1,9 +1,9 @@
 package org.luke.newYearCountDown;
 
-import org.bukkit.Bukkit;
-import org.bukkit.ChatColor;
-import org.bukkit.SoundCategory;
+import org.bukkit.*;
+import org.bukkit.entity.Firework;
 import org.bukkit.entity.Player;
+import org.bukkit.inventory.meta.FireworkMeta;
 import org.bukkit.plugin.java.JavaPlugin;
 import org.bukkit.scheduler.BukkitRunnable;
 
@@ -11,16 +11,11 @@ import java.text.DecimalFormat;
 import java.time.Duration;
 import java.time.LocalDateTime;
 import java.time.ZoneId;
-import java.time.temporal.ChronoUnit;
-import java.util.concurrent.Executors;
-import java.util.concurrent.ScheduledExecutorService;
-import java.util.concurrent.TimeUnit;
 
 import static org.luke.takoyakiLibrary.TakoUtility.toColor;
 
 public final class NewYearCountDown extends JavaPlugin {
-    private final ScheduledExecutorService scheduler = Executors.newSingleThreadScheduledExecutor();
-    LocalDateTime time = LocalDateTime.of(2025, 1, 1, 0, 0);
+    public static final LocalDateTime time = LocalDateTime.of(2025, 1, 1, 0, 0);
 
     @Override
     public void onEnable() {
@@ -33,7 +28,7 @@ public final class NewYearCountDown extends JavaPlugin {
         // Plugin shutdown logic
     }
 
-    private Duration getLeftMillis() {
+    public static Duration getLeftMillis() {
         LocalDateTime now = LocalDateTime.now(ZoneId.systemDefault());
 
         // 残り時間をミリ秒で計算
@@ -51,14 +46,28 @@ public final class NewYearCountDown extends JavaPlugin {
             @Override
             public void run() {
                 long millisUntilCountdownStart = getLeftMillis().toMillis();
+                Duration left = getLeftMillis();
 
                 LocalDateTime now = LocalDateTime.now(ZoneId.systemDefault());
-                if(Duration.between(now, time).toMillis() >= 1000*30) {
+                if(millisUntilCountdownStart >= 1000*30) {
                     long current_hour = now.getHour();
                     long current_minute = now.getMinute();
                     long current_second = now.getSecond();
+
+                    String barMessage = "&a現在時刻: "+ current_hour +"時 "
+                            + current_minute +"分 "
+                            + current_second + "秒 &f|&c&l 新年まで ";
+                    if(left.toHours() >= 1)  {
+                        barMessage += left.toHours() + "時間 "
+                                + left.toMinutesPart() +"分";
+                    } else if(left.toMinutes() < 60){
+                        barMessage += left.toMinutes() +"分 ";
+                    } else  {
+                        barMessage += left.toSeconds() +"秒";
+                    }
+
                     for(Player player: Bukkit.getOnlinePlayers()) {
-                        player.sendActionBar(toColor("&6&l現在時刻: "+ current_hour +"時 "+ current_minute +"分 " + current_second + "秒"));
+                        player.sendActionBar(toColor(barMessage));
                     }
                 }
 
@@ -81,17 +90,27 @@ public final class NewYearCountDown extends JavaPlugin {
                                 title = "&b&l" + formattedTime;
                             }
 
+                            if(previousSecond != second) {
+                                previousSecond = second;
+                                PlaySoundEffect();
+                            }
                             player.sendTitle(toColor(title), toColor("&c===&6新年へのカウントダウン&c==="), 0, 20 * 2, 0);
                         }
                     } else {
-
-
                         if(previousSecond != second) {
                             previousSecond = second;
-                            if(millisUntilCountdownStart <= 1000 * 21) {
+                            if(millisUntilCountdownStart <= 1000 * 61) {
+                                PlaySoundEffect();
                                 Bukkit.broadcastMessage(ChatColor.YELLOW + "新年まであと " + ChatColor.RED + second + ChatColor.YELLOW + " 秒！");
-                            } else if(second % 60 == 0) { //1分に一度メッセージを送信
+                            } else if(second % 60 == 0 && minute <= 10) { //1分に一度メッセージを送信(10分前から開始)
+                                PlaySoundEffect();
                                 Bukkit.broadcastMessage(toColor("&e新年まであと &c"+ minute +"&e分 &c"+ getLeftMillis().toSecondsPart() +"&c秒"));
+                            } else if(second % (60 * 15) == 0 && minute < 60) { // 15分に一度メッセージを送信
+                                PlaySoundEffect();
+                                Bukkit.broadcastMessage(toColor("&e新年まであと &c"+ minute +"&e分 &c"+ getLeftMillis().toSecondsPart() +"&c秒"));
+                            } else if(second % (60 * 60) == 0 && minute >= 60) {
+                                PlaySoundEffect();
+                                Bukkit.broadcastMessage(toColor("&e新年まであと &c"+ left.toHours() +"&e時間 &c"+ left.toMinutesPart() +"&c分"));
                             }
                         }
                     }
@@ -103,15 +122,55 @@ public final class NewYearCountDown extends JavaPlugin {
         }.runTaskTimer(this, 0, 2); // 2 ticks = 0.1秒
     }
 
+    private void PlaySoundEffect() {
+        for (Player player : Bukkit.getOnlinePlayers()) {
+            player.playSound(
+                    player.getLocation(),                 // 再生する位置（プレイヤーの位置）
+                    Sound.UI_BUTTON_CLICK,                          // サウンドの名前
+                    1.0f,                                 // 音量
+                    1.0f                                  // ピッチ
+            );
+        }
+    }
+
+    private void launchFireworks() {
+        for(Player player : Bukkit.getOnlinePlayers()) {
+            player.getWorld().spawn(player.getLocation(), Firework.class, firework -> {
+                FireworkMeta meta = firework.getFireworkMeta();
+                meta.addEffect(FireworkEffect.builder()
+                        .withColor(Color.RED)
+                        .withColor(Color.YELLOW)
+                        .withFade(Color.ORANGE)
+                        .with(FireworkEffect.Type.BALL_LARGE)
+                        .trail(true)
+                        .flicker(true)
+                        .build());
+                meta.setPower(1); // パワーを設定
+                firework.setFireworkMeta(meta);
+            });
+
+            player.spawnParticle(Particle.EXPLOSION_EMITTER, player.getLocation(), 1, 0, 0, 0, 0.1);
+        }
+    }
+
     private void Complete() {
-        Bukkit.broadcastMessage(ChatColor.GOLD + "あけましておめでとうございます！");
+        Bukkit.broadcastMessage(ChatColor.GOLD + "あけましておめでとうございます！ : " +  (time.getYear() + 1) + "年");
         for(Player player : Bukkit.getOnlinePlayers()) {
             player.sendTitle(toColor("&6&lあけおめ"), (time.getYear() + 1)+"年度もよろしく～～～～", 20, 20 * 20, 20);
         }
+        for (Player player : Bukkit.getOnlinePlayers()) {
+            player.playSound(
+                    player.getLocation(),                 // 再生する位置（プレイヤーの位置）
+                    Sound.ENTITY_DRAGON_FIREBALL_EXPLODE, // サウンドの名前
+                    1.1f,                                 // 音量
+                    1.0f                                  // ピッチ
+            );
+        }
         playBGMToAllPlayers();
+        launchFireworks();
     }
 
-    public void playBGMToAllPlayers() {
+    public static void playBGMToAllPlayers() {
         String customSound = "newyear_pack:music_disc.newyearbgm"; // データパックで登録したサウンド名
 
         // サーバーにいる全プレイヤーにBGMを再生
